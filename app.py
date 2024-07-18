d5d81f421dc087fcf2a7ed14fdf7d0ececeaec8d
dennis71201 <dennis71201@gmail.com>
2024-07-18 20:38:28 +0800

    init

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
+import requests
+import json
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
 
+url = "http://192.168.0.231:3001/api/v1/workspace/kebbi-demo/chat"#注意t1的值，每個人的不一樣
+api_key = "855077P-2YW420X-JDS7907-DATXR78"#自訂，每個人的不一樣
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json"
+}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -23,10 +33,24 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    data = {
+    "message": event.message.text,
+    "mode": "query"
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    print(response.json()['textResponse'])
+
+    message = TextSendMessage(text=response.json()['textResponse'])
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    
+
+
+
+
+
+
