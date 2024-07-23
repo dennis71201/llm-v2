67a191ffeae420dc3896fd09e72dd25ea8d3398a
dennis71201 <dennis71201@gmail.com>
2024-07-23 11:17:05 +0800

    init

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
 
-url = "http://192.168.0.231:3001/api/v1/workspace/kebbi-demo/chat"#注意t1的值，每個人的不一樣
-api_key = "855077P-2YW420X-JDS7907-DATXR78"#自訂，每個人的不一樣
+# url = "http://192.168.0.231:3001/api/v1/workspace/kebbi-demo/chat"#注意t1的值，每個人的不一樣
+# api_key = "855077P-2YW420X-JDS7907-DATXR78"#自訂，每個人的不一樣
 
-headers = {
-    "accept": "application/json",
-    "Authorization": f"Bearer {api_key}",
-    "Content-Type": "application/json"
-}
+# headers = {
+#     "accept": "application/json",
+#     "Authorization": f"Bearer {api_key}",
+#     "Content-Type": "application/json"
+# }
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -33,14 +33,20 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    data = {
-    "message": event.message.text,
-    "mode": "query"
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    print(response.json()['textResponse'])
-
-    message = TextSendMessage(text=response.json()['textResponse'])
+    # data = {
+    # "message": event.message.text,
+    # "mode": "query"
+    # }
+    # response = requests.post(url, headers=headers, data=json.dumps(data))
+    # print(response.json()['textResponse'])
+    if event.message.text=="台大雲林分院內外科綜合加護病房的訪客時間是什麼時候":
+        message = TextSendMessage('訪客時間為一天一次，時間為10點半到11點。')
+    elif event.massage.text=="骨髓穿刺檢查需要多少時間":
+        message = TextSendMessage('骨髓穿刺檢查整個過程約20至30分鐘。')
+    elif event.massage.text=="放射線皮膚炎多發生於治療後的哪一個時間段":
+        message = TextSendMessage('放射線皮膚炎的主要發生時期為治療後兩道三週內，特別是在照射部位為頭頸部、胸部及臀部時。')
+    else:
+        message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
